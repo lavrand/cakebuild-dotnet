696a6fc4f2c1294d2dfefde562c90bbfa46cf501
Danil <dbrenner@vente-privee.com>
2018-12-20 17:03:55 +0100

    Checkout git

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mono:latest
 
-RUN apt-get update && apt-get -y install libicu-dev \
+RUN apt-get update && apt-get -y install libicu-dev && apt-get install -y git \
     && curl -L -o /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh \
     && chmod +x tmp/dotnet-install.sh \
     && ./tmp/dotnet-install.sh -c Current \
--- runbuild.sh
+++ runbuild.sh
@@ -6,6 +6,9 @@ export PATH=$PATH:/root/.dotnet
 [[ -z "${BUILD_SCRIPT}" ]] && script='./build.sh' || script="${BUILD_SCRIPT}"
 [[ -z "${BUILD_TARGET}" ]] && target='Build' || target="${BUILD_TARGET}"
 
-cd ${sourcePath} 
+cd ${sourcePath}
+if [ -n "${GIT_BRANCH}" ]; then
+    git checkout -b ${GIT_BRANCH} --track ${GIT_BRANCH}
+fi
 chmod +x $script
 ./$script -target=$target
