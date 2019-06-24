4de40accd10bbd686281bf7020ce6ce2f7360737
Danil <dbrenner@vente-privee.com>
2019-06-24 07:02:17 +0300

    Git checkout fix

--- runbuild.sh
+++ runbuild.sh
@@ -3,12 +3,12 @@
 export PATH=$PATH:/root/.dotnet 
 
 [[ -z "${SOURCE_PATH}" ]] && sourcePath='/source' || sourcePath="${SOURCE_PATH}"
-[[ -z "${BUILD_SCRIPT}" ]] && script='./build.sh' || script="${BUILD_SCRIPT}"
+[[ -z "${BUILD_SCRIPT}" ]] && script='build.sh' || script="${BUILD_SCRIPT}"
 [[ -z "${BUILD_TARGET}" ]] && target='Build' || target="${BUILD_TARGET}"
 
 cd ${sourcePath}
 if [ -n "${GIT_BRANCH}" ]; then
-    git checkout -b ${GIT_BRANCH} --track ${GIT_BRANCH}
+    git checkout ${GIT_BRANCH}
 fi
 chmod +x $script
 ./$script -target=$target
